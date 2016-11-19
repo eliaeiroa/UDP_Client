84cf7b502a2a3e53c37be60d56b9ccd54e5cc43b
Elia Eiroa <EliaEiroa@elias-MacBook-Pro.local>
2016-11-18 16:20:01 -0800

    UDP Client communicating with Server

---
+++ UDPClient.java
@@ -0,0 +1,73 @@
+/**
+*	UDP Client Program
+*	Connects to a UDP Server
+*	Receives a line of input from the keyboard and sends it to the server
+*	Receives a response from the server and displays it.
+*
+*	@author: Elia Eiroa
+@	version: 2.1
+*/
+
+import java.io.*;
+import java.net.*;
+
+class UDPClient {
+    public static void main(String args[]) throws Exception
+    {
+
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+      DatagramSocket clientSocket = new DatagramSocket();
+
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+
+      while(true){
+        System.out.print("Input: ");
+        String sentence = inFromUser.readLine();
+
+
+        if(sentence.toLowerCase().equals("goodbye"))
+        {
+
+      sendData = sentence.getBytes();
+
+	    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+      clientSocket.send(sendPacket);
+
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+      clientSocket.receive(receivePacket);
+
+      String modifiedSentence = new String(receivePacket.getData());
+
+      System.out.println("FROM SERVER:" + modifiedSentence);
+      clientSocket.close();
+      break;
+    }
+    else{
+      sendData = sentence.getBytes();
+
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+      clientSocket.send(sendPacket);
+
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+      clientSocket.receive(receivePacket);
+
+      String modifiedSentence = new String(receivePacket.getData());
+
+      System.out.println("FROM SERVER:" + modifiedSentence);
+
+    }
+    }
+  }
+
+
+
+
+}
